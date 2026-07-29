6542748b972e74a79526481db8a9e16fb6a36de4
Pawikan Boonnaum <160022244+FriedHamCheese@users.noreply.github.com>
2026-07-29 14:33:34 +0700

    mono audio out proto

---
+++ STM32/Core/Src/main.c
@@ -0,0 +1,250 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "tim.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#define _USE_MATH_DEFINES
+#include <math.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+const float samplerate = 48000.0f;
+const float audio_power_ratio = 0.1f;
+
+struct Waveform{
+	float waveform_completion_ratio;
+	float waveform_completion_increment;
+};
+struct Waveform wave_1 = {0.00, 100.0f/samplerate};
+
+float frequencies[] = {
+	100.0f,
+	200.0f,
+	500.0f,
+	1000.0f
+};
+
+uint8_t frequency_id = 0;
+uint8_t point_generator_id = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+void set_audio_output_value(uint16_t value){
+	TIM3->CCR1 = (value >> 8);
+}
+
+uint16_t get_sine_point(float waveform_completion_ratio){
+	const float completed_waveform_radians = M_PI * 2.0f;
+	const float uint16_maxf = UINT16_MAX;
+	const float make_min_as_zero = 1.0f;
+	const float waveform_max_as_one = 1.0f/2.0f;
+	return (sinf(completed_waveform_radians * waveform_completion_ratio) + make_min_as_zero) * waveform_max_as_one * uint16_maxf * 350.0f/255.0f * audio_power_ratio;
+}
+
+uint16_t get_triangle_point(float waveform_completion_ratio){
+	const float uint16_maxf = UINT16_MAX;
+
+	if(waveform_completion_ratio < 0.25f)
+		return 0.5f + (waveform_completion_ratio * 2.0f) * uint16_maxf * audio_power_ratio * 350.0f/255.0f;
+	if(waveform_completion_ratio < 0.75f)
+		return 1.5f + (waveform_completion_ratio * -2.0f) * uint16_maxf * audio_power_ratio * 350.0f/255.0f;
+	return -1.5f + (waveform_completion_ratio * 2.0f) * uint16_maxf * audio_power_ratio * 350.0f/255.0f;
+}
+
+uint16_t get_square_point(float waveform_completion_ratio){
+	return waveform_completion_ratio < 0.5f ? UINT16_MAX : 0;
+}
+
+
+uint16_t (*point_generators[3])(float) = {
+  get_sine_point,
+  get_triangle_point,
+  get_square_point
+};
+
+void next_audio_sample(TIM_HandleTypeDef* htim){
+	wave_1.waveform_completion_ratio += wave_1.waveform_completion_increment;
+	if(wave_1.waveform_completion_ratio >= 1.0f)
+		wave_1.waveform_completion_ratio -= 1.0f;
+	set_audio_output_value(point_generators[point_generator_id](wave_1.waveform_completion_ratio));
+}
+
+float get_waveform_completion_increment(float frequency, float samplerate){
+	return frequency / samplerate;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_RegisterCallback(&htim4, HAL_TIM_PERIOD_ELAPSED_CB_ID, next_audio_sample);
+  HAL_TIM_Base_Start_IT(&htim4);
+  set_audio_output_value(UINT16_MAX / 2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
